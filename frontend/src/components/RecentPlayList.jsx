d389db4979e6c86a582fac093a52bad6836a100f
STS369 <satoshikikuchi778@gmail.com>
2025-10-04 13:09:31 +0900

    RecentPlayList.jsx_change_list

--- frontend/src/components/RecentPlayList.jsx
+++ frontend/src/components/RecentPlayList.jsx
@@ -2,12 +2,12 @@
 import React from "react";
 import SongItem from "./SongItem";
 
-export default function RecentPlayList({ tracks, onQuickAdd }) {
+export default function RecentPlayList({ tracks, onQuickAdd, onAddSelected }) {
   if (!tracks?.length) return <div>最近聴いた曲がありません。</div>;
   return (
     <div className="list">
       {tracks.map(t => (
-        <SongItem key={t.id} track={t} onQuickAdd={onQuickAdd} />
+        <SongItem key={t.id} track={t} onQuickAdd={onQuickAdd} onAddSelected={onAddSelected} />
       ))}
     </div>
   );
